bbeee767fa1a013ad1f252973934dbfc4fc26278
Tim <book9902@yandex.ru>
2023-01-27 09:24:53 +0300

    mini fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.util.List;
@@ -11,17 +12,18 @@ public class Main {
 
     public static ObjectMapper mapper = new ObjectMapper();
 
-    public static void main(String[] args) throws IOException {
-        var httpClient = HttpClients.createDefault();
+    public static void main(String[] args) {
         var httpGet = new HttpGet(IP);
-        try (var response = httpClient.execute(httpGet)
+        try (var httpClient = HttpClients.createDefault()
         ) {
+            var response = httpClient.execute(httpGet);
+           // var entity = response.getEntity();
+           // System.out.println(EntityUtils.toString(entity));
             var infoID = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<InfoID>>() {
             });
             infoID.stream().filter(value -> value.getUpVotes() > 0).forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        httpClient.close();
     }
 }
